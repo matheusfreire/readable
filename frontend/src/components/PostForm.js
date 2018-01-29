f51e44cf9e9eef3c859cd0afa81570a8533e95d5
Matheus Freire <matheus.sfreire@gmail.com>
2018-01-29 20:03:11 -0200

    Post Form

--- frontend/src/components/PostForm.js
+++ frontend/src/components/PostForm.js
@@ -3,7 +3,46 @@ import { connect } from 'react-redux';
 import { add } from '../actions/post';
 import { Field, reduxForm } from 'redux-form';
 import {getAllCategories} from '../actions/categories';
-import { Redirect } from 'react-router'
+import { Redirect } from 'react-router';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const renderTextField = ({
+    input,
+    label,
+    meta: { touched, error },
+    ...custom
+  }) => (
+    <TextField
+      hintText={label}
+      floatingLabelText={label}
+      errorText={touched && error}
+      {...input}
+      {...custom}
+    />
+  )
+
+  const renderSelectField = ({
+    input,
+    label,
+    meta: { touched, error },
+    children,
+    ...custom
+  }) => (
+    <SelectField
+      floatingLabelText={label}
+      errorText={touched && error}
+      {...input}
+      onChange={(event, index, value) => input.onChange(value)}
+      children={children}
+      {...custom}
+    />
+  )
+  const style = {
+    margin: 12,
+  };
 
 class PostForm extends Component {
 
@@ -23,6 +62,7 @@ class PostForm extends Component {
         })
     }
     
+    
     render() {
         
         if (this.state.redirectToHome) {
@@ -34,45 +74,27 @@ class PostForm extends Component {
             return (
                 <form onSubmit={handleSubmit(this.submit)}>
                     <div>
-                        <label>Title</label>
-                        <div>
-                            <Field name="title" component="input" type="text" placeholder="Title" />
-                        </div>
+                        <Field name="title" component={renderTextField} label="Title"/>
                     </div>
                     <div>
-                        <label>Author</label>
-                        <div>
-                            <Field name="author" component="input" type="text" placeholder="Title" />
-                        </div>
+                        <Field name="author" component={renderTextField} label="Author"/>
                     </div>
     
                     <div>
-                        <label>Body</label>
-                        <div>
-                            <Field name="body" component="input" type="textarea" placeholder="Body" />
-                        </div>
+                        <Field name="body" component={renderTextField} label="Body" multiLine={true}/>
                     </div>
     
                     <div>
-                        <label>Categories</label>
-                        <div>
-                            <Field name="category" component="select">
-                                <option value="">Select a category...</option>
-                                {this.props.categories.map((c, idx) => (
-                                    <option value={c.path} key={idx}>
-                                        {c.name}
-                                    </option>
-                                ))}
-                            </Field>
-                        </div>
+                        <Field name="category" component={renderSelectField} label="Category">
+                            {this.props.categories.map((c, idx) => (
+                                <MenuItem value={c.path} key={idx} primaryText={c.name} />
+                            ))}
+                        </Field>
                     </div>
                     <div>
-                        <button type="submit" disabled={submitting}>
-                                Submit
-                        </button>
-                            <button type="button" disabled={submitting} onClick={reset}>
-                                Clear Values
-                        </button>
+                        <RaisedButton label="Submit" style={style} type="submit"/>
+                        <RaisedButton label="Submit" style={style} type="button" onClick={reset}/>
+                        
                     </div>
                 </form>
             )
--- frontend/src/components/SideMenu.js
+++ frontend/src/components/SideMenu.js
@@ -1,34 +1,40 @@
-import React,{Component } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Drawer from 'material-ui/Drawer';
+
 import MenuItem from 'material-ui/MenuItem';
 import RaisedButton from 'material-ui/RaisedButton';
 
-import {close} from '../actions/sidemenu';
+import { close } from '../actions/sidemenu';
+import {getAllCategories} from '../actions/categories';
 
 class SideMenu extends Component {
 
 
-  close =() => {
-    this.props.close();
-  }
+    close = () => {
+        this.props.close();
+    }
 
-  render() {
-    return (
-      <div>
-        <Drawer open={this.props.open}>
-          <MenuItem>Menu Item</MenuItem>
-          <MenuItem>Menu Item 2</MenuItem>
-          <MenuItem onClick={() => this.close()}>Close menu</MenuItem>
-        </Drawer>
+    render() {
+        return (
+            <div>
+                <Drawer open={this.props.open}>
+                    <MenuItem>New post</MenuItem>
+                    <MenuItem>Topics
+                        {this.props.categories.map((category) => (
+                            <MenuItem key={category.path} value={category.path} primaryText={category.name} />
+                        ))}
+                    </MenuItem>
+                    <MenuItem onClick={() => this.close()}>Close menu</MenuItem>
+                </Drawer>
 
 
-        
-      </div>
-    );
-  }
+
+            </div>
+        );
+    }
 }
 
-const mapStateToProps = state => ({ open: state.sideMenuReducer.open })
-const mapDispatchToProps = { close }
+const mapStateToProps = state => ({ open: state.sideMenuReducer.open,categories: state.categoriesReducer.categories })
+const mapDispatchToProps = { close, getAllCategories }
 export default connect(mapStateToProps, mapDispatchToProps)(SideMenu)
